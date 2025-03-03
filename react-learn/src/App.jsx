c8b4cf949f02db8fac5f01be63b8c5ffaa033646
Rahul Vikram <75101301+rahulvikram@users.noreply.github.com>
2025-03-03 15:43:26 -0800

    changes

--- react-learn/src/App.jsx
+++ react-learn/src/App.jsx
@@ -3,21 +3,37 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { InfoCard } from './components/InfoCard'
 
+// formats the date to be displayed as a string
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long' }
+  ).format(date);
+}
+
 // defines a react component of name App
 export default function App() {
   const [count, setCount] = useState("Append to me! ")
 
+  // defines an array of project titles and contents for dynamic rendering
+  const projectTitles = ["RunMetrics Visualizer", "Beaver Board"]
+  const projectContents = ["A handy developer tool for storing function runtime data and graphing it via GUI!", "An app where student users can keep track of their upcoming assignments, projects and exams"]
+  const projectUrls = ["https://github.com/rahulvikram/RunMetrics-Visualizer", "https://github.com/rahulvikram/beaver-board"]
+  
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Happy {formatDate(new Date())}</h1>
       <img src={reactLogo} alt="React Logo" />
       <button onClick={() => setCount(count + "Appended! ")}>Click me</button>
       <p>Count: {count}</p>
 
       <h3>My Work</h3>
       <div className="projects">
-        <InfoCard title="RunMetrics Visualizer" content="A handy developer tool for storing function runtime data and graphing it via GUI!" />
-        <InfoCard title="Beaver Board" content="An app where student users can keep track of their upcoming assignments, projects and exams" />
+        {/* map over projectTitles and projectContents to create a new InfoCard for each */}
+        {/* passing props to the InfoCard component */}
+        {projectTitles.map((title, index) => (
+          <InfoCard key={index} title={title} content={projectContents[index]} url={projectUrls[index]} />
+        ))}
       </div>
     </>
   )
--- react-learn/src/components/InfoCard.jsx
+++ react-learn/src/components/InfoCard.jsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react'
 import '../App.css'
 
 // InfoCard is a parameterised component so we can dynamically pass in different values for title and content
-export function InfoCard({ title, content }) {
+// title and content are props passed in from the parent component
+export function InfoCard({ title, content, url }) {
     return (
-        <div className="info-card">
-        <h4>{title}</h4>
-        <p>{content}</p>
-        </div>
+        <a href={url} className="info-card-link" target="_blank" rel="noopener noreferrer">
+            <div className="info-card hover-animation">
+                <h4>{title}</h4>
+                <p>{content}</p>
+            </div>
+        </a>
     )
 }
 
